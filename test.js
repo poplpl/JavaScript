b687fe9c9ec1c989a12f51c4d35c572736554ea7
poplpl90 <poplpl90@gmail.com>
2020-03-19 21:01:49 +0900

    js

---
+++ test.js
@@ -0,0 +1,6 @@
+ var num =0; 
+ console.log('aaaaaaaaaaaaaaa') 
+ document.write("head 태그 내 실행 순서: "+num+"<br>") 
+document.write("head 태그 내 실행 순서: "+num+"<br>") 
+var age =23; 
+document.write("<div style='color: red; font-size: 24px;'>" +"자바 스크립트</div>")
